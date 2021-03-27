094c8a3396071616c50ebfc474f98b5cef354a72
max7290599 <atroshschanka.maksim@gmail.com>
2021-03-28 01:35:49 +0300

    refactor: add BEM selectors according to rule 2.1 in html-and-css.md

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ var createNewTaskElement=function(taskString){
     label.className='task label';
 
     //Each elements, needs appending
-    listItem.className="task-list-elem";
+    listItem.className="task-list__elem";
     checkBox.type="checkbox";
     checkBox.className="inp-elem";
     editInput.type="text";
@@ -88,11 +88,11 @@ var editTask=function(){
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
     var editBtn=listItem.querySelector(".edit");
-    var containsClass=listItem.classList.contains("edit-mode");
-    //If class of the parent is .edit-mode
+    var containsClass=listItem.classList.contains("task-list__edit");
+    //If class of the parent is .task-list__edit
     if(containsClass){
 
-        //switch to .edit-mode
+        //switch to .task-list__edit
         //label becomes the inputs value.
         label.innerText=editInput.value;
         editBtn.innerText="Edit";
@@ -101,8 +101,8 @@ var editTask=function(){
         editBtn.innerText="Save";
     }
 
-    //toggle .edit-mode on the parent.
-    listItem.classList.toggle("edit-mode");
+    //toggle .task-list__edit on the parent.
+    listItem.classList.toggle("task-list__edit");
 };
 
 
